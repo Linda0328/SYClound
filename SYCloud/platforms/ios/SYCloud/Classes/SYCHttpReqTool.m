036b9aea19b72f0c2c45c60e15da63b8b804c938
Linda0328 <1196992912@qq.com>
2017-03-13 10:47:10 +0800

    添加http请求共用tool

---
+++ SYCloud/platforms/ios/SYCloud/Classes/SYCHttpReqTool.m
@@ -0,0 +1,74 @@
+//
+//  SYCHttpReqTool.m
+//  SYCloud
+//
+//  Created by 文清 on 2017/3/13.
+//
+//
+
+#import "SYCHttpReqTool.h"
+#import "SYCSystem.h"
+#import "SYCShareVersionInfo.h"
+static NSString * const SYVersionParam = @"/app_resources/app/version.json?_";
+static NSString * const SYMainParam = @"/app_resources/app/main.json?_";
+@implementation SYCHttpReqTool
++(NSDictionary*)VersionInfo{
+    NSString *baseURL = [SYCSystem baseURL];
+    NSString *reqUrl = [baseURL stringByAppendingFormat:@"%@%@",SYVersionParam,[SYCSystem secondsForNow]];
+    NSURL *url = [NSURL URLWithString:reqUrl];
+    NSURLRequest *request = [[NSURLRequest alloc]initWithURL:url];
+   
+    NSURLResponse *response = nil;
+    NSError *error = nil;
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    if (error) {
+        NSLog(@"---版本信息请求出错---%@",[error description]);
+        return nil;
+    }
+    NSError *err = nil;
+    NSLog(@"responseMain : %@",response);
+    NSString *backData = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+    backData = [backData stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+    backData = [backData stringByReplacingOccurrencesOfString:@"\r" withString:@""];
+    backData = [backData stringByReplacingOccurrencesOfString:@"\t" withString:@""];
+    NSLog(@"backDataMain : %@",backData);
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:[backData dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:&err];
+    if (err) {
+        NSLog(@"---数据解析出错---%@",[err description]);
+    }else{
+        NSLog(@"----解析结果--- : %@",dic);
+    }
+    return dic;
+    
+}
++(NSDictionary*)MainData{
+    NSString *baseURL = [SYCSystem baseURL];
+    NSString *reqUrl = [baseURL stringByAppendingFormat:@"%@%@",SYMainParam,[SYCShareVersionInfo sharedVersion].pageVersion];
+    NSURL *url = [NSURL URLWithString:reqUrl];
+    NSURLRequest *request = [[NSURLRequest alloc]initWithURL:url];
+    NSURLResponse *response = nil;
+    NSError *error = nil;
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    if (error) {
+        NSLog(@"---版本信息请求出错---%@",[error description]);
+        return nil;
+    }
+    NSError *err = nil;
+    NSLog(@"responseMain : %@",response);
+    
+    NSString *backData = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+    backData = [backData stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+    backData = [backData stringByReplacingOccurrencesOfString:@"\r" withString:@""];
+    backData = [backData stringByReplacingOccurrencesOfString:@"\t" withString:@""];
+    NSLog(@"backDataMain : %@",backData);
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:[backData dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:&err];
+    if (err) {
+        NSLog(@"---数据解析出错---%@",[err description]);
+    }else{
+        NSLog(@"----解析结果--- : %@",dic);
+    }
+    return dic;
+    
+}
+
+@end
